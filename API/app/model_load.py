53c300361badd203416f196194582e50974d8ddb
AKISHPOTHURI <akishpothuri@gmail.com>
2023-12-22 14:31:52 +0530

    model 2 loaded

--- API/app/model_load.py
+++ API/app/model_load.py
@@ -3,5 +3,5 @@ import keras
 class load_model():
     @staticmethod
     def load():
-        model = keras.models.load_model('app\sentimentModel\model1.h5')
+        model = keras.models.load_model('app\sentimentModel\model2.h5')
         return model
