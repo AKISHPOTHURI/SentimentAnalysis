0547d5ffc15e11134157cdf475c71ce370e9459b
AKISHPOTHURI <akishpothuri@gmail.com>
2024-01-06 02:16:34 +0530

    evaluation status file not saving

--- SentimentAPI/src/Sentiment/components/model_evaluation.py
+++ SentimentAPI/src/Sentiment/components/model_evaluation.py
@@ -1,8 +1,10 @@
 import os
 from Sentiment import logger
 import keras
-from sklearn.metrics import accuracy_score,recall_score,precision_score,f1_score
-
+from sklearn import metrics
+from nltk.tokenize import word_tokenize
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
 from Sentiment.entity.config_entity import ModelEvaluationConfig
 
 class ModelEvaluation:
@@ -13,7 +15,7 @@ class ModelEvaluation:
         '''
         Model Loading from the artifacts
         '''
-        model = keras.models.load_model(os.path.join(self.config.model,"LSTMModel"+"V1"+".h5"))
+        model = keras.models.load_model(os.path.join(self.config.model,"LSTMModel"+"V2"+".h5"))
         return model
     
     def predictTest(self,testSentences,model):
@@ -22,15 +24,43 @@ class ModelEvaluation:
         '''
         return model.predict(testSentences)
     
+    def sentimentClassify(self,predictions):
+        prediction = []
+        for i in predictions:
+            if i > 50:
+                prediction.append(1)
+            else:
+                prediction.append(0)
+        return prediction
+    
+    def TextToNumeric(self,testSentences):
+        '''
+        Converting the text to numeric
+        '''
+        try:
+            vocab_size = 3000 # choose based on statistics
+            oov_tok = ''
+            max_length = 200
+            padding_type = 'post'
+            tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+            tokenizer.fit_on_texts(testSentences)
+            word_index = tokenizer.word_index
+            # convert Test dataset to sequence and pad sequences
+            testSequences = tokenizer.texts_to_sequences(testSentences)
+            testPadded = pad_sequences(testSequences, padding=padding_type, maxlen=max_length)
+            return testPadded
+        except Exception as e:
+            raise e
+    
     def EvaluationMetrics(self,predictions,testLabels):
         '''
         Finding evaluation metrics
         '''
         try:
-            accuracyScore = accuracy_score(testLabels,predictions)
-            recallScore = recall_score(testLabels,predictions)
-            precisionScore = precision_score(testLabels,predictions)
-            f1Score = f1_score(testLabels,predictions)
+            accuracyScore = metrics.accuracy_score(testLabels,predictions,average='weighted',zero_division=0)
+            recallScore = metrics.recall_score(testLabels,predictions,average='weighted',zero_division=0)
+            precisionScore = metrics.precision_score(testLabels,predictions,average='weighted',zero_division=0)
+            f1Score = metrics.f1_score(testLabels,predictions,average='weighted',zero_division=0)
             return accuracyScore,recallScore,precisionScore,f1Score
         except Exception as e:
             raise e
@@ -43,7 +73,7 @@ class ModelEvaluation:
             Evaluation_status = None
 
             all_files = os.listdir(os.path.join("artifacts","model_evaluation"))
-
+    
             for file in all_files:
                 if file not in self.config.ALL_REQUIRED_FILES:
                     Evaluation_status = False
@@ -51,6 +81,7 @@ class ModelEvaluation:
                         f.write(f"Evaluation status: {Evaluation_status}")
                 else:
                     Evaluation_status = True
+                    
                     with open(self.config.STATUS_FILE, 'w') as f:
                         f.write(f"Evaluation status: {Evaluation_status}"+",")
                         f.write(f"accuracyScore: {accuracyScore}"+",")
